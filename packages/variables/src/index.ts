b32879e170ec1137da03148c64ec55fd5c153515
Alapan Chatterjee <alapan.chatterjee@cyware.com>
2023-07-28 20:23:32 +0530

    chore: complete basic typescript examples

---
+++ packages/variables/src/index.ts
@@ -0,0 +1,10 @@
+const age = 6;
+
+const RANDOM_WAIT_TIME =
+  Math.round(Math.random() * 500) + 500;
+let startTime = new Date();
+let endTime: Date;
+
+setTimeout(() => {
+  endTime = new Date();
+}, RANDOM_WAIT_TIME);
